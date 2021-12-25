13f3243cba274dabd0b587f32a1c8567f4e94bc9
LoicULB <Loic.Quivron@ulb.be>
2021-12-25 08:56:07 +0100

    Import A2C code from tutorial

--- A2C/ActorCriticModel.py
+++
@@ -1,8 +0,0 @@
-import os
-import tensorflow.keras
-from tensorflow.keras.layers import Dense
-
-class ActorCriticNetwork(keras.Model):
-    def __init__(self, n_actions, fc1_dims=1024, fc2_dims=512, name="actor_critic", chkpt_dir="tmp/actor_critic"):
-        super(ActorCriticNetwork, self).__init__()
-        self
