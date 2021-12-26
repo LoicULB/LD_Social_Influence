658c17393bbb34b7eb33a409480871ce42054efb
Andrea Gonzato <gonzatoandrea98@gmail.com>
2021-12-26 18:39:11 +0100

    env now working

--- test/main.py
+++ test/main.py
@@ -4,9 +4,22 @@ from harvest import HarvestEnv
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    env = HarvestEnv()
-    print(env.action_space)
+    env = HarvestEnv(num_agents=1)
+    env.reset()
+
     env.setup_agents()
-    print()
-    env.render()
+
+
+    print("orientation agent 0: ",env.agents["agent-0"].orientation) # TEST
+
+
+    # render the current env
+    env.render("current-env at time 0")
+
+    dic = {"agent-0": 3}
+
+
+    env.step(dic)
+    # render the current env
+    env.render("current-env at time 1")
 
