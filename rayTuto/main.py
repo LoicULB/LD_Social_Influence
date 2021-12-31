0edcaf28944e270157c7a7e62b06e2a7b2c3a620
Loic <45242@etu.he2b.be>
2021-12-31 15:00:10 +0100

    Try to do like paper but fail, problem with observation space

--- rayTuto/main.py
+++ rayTuto/main.py
@@ -1,6 +1,8 @@
 
 import ray
 from ray.rllib import agents
+from ray.rllib.models import ModelCatalog
+from rayTuto.models.BaselineModel import BaselineModel
 from social_dilemma.environment.harvest import HarvestEnv
 from ray.tune.registry import register_env
 def pretty_print(result):
@@ -11,17 +13,30 @@ def env_creator(_):
     return  HarvestEnv(num_agents=2)
 if __name__ == "__main__":
     ray.init() # Skip or set to ignore if already called
+    env = env_creator
+    register_env("harvest", env)
+    model_name = "baseline"
+    ModelCatalog.register_custom_model(model_name, BaselineModel)
+
+    obs_space = env.observation_space
+    act_space = env.action_space
+
+    """
+    def gen_policy():
+        return None, obs_space, act_space, {"custom_model": model_name}
+    """
     config = {'gamma': 0.9,
           'lr': 1e-2,
           'num_workers': 2,
-          'train_batch_size': 1000,
-          'model': {
-              'fcnet_hiddens': [128, 128]
-          }}
-    env = env_creator
-    register_env("harvest", env)
+            'model': {
+              "custom_model": "baseline",
+              # Extra kwargs to be passed to your model's c'tor.
+              "custom_model_config": {},
+            }
+          }
+
     trainer = agents.a3c.A2CTrainer(env="harvest", config=config)
 
-    for i in range (10):
+    for i in range (1):
         results = trainer.train()
         pretty_print(results)
